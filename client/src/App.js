ab15c8fb279190a3c27178adfa478a7bc9ce4ecc
Amansinghal904 <amansinghal904@gmail.com>
2024-06-09 21:23:37 +0530

    added routes for register and home page

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,19 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import Navibar from './components/Navibar.js';
 import Home from './components/Home.js';
+import Register from './components/Register.js';
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 function App() {
   return (
     <>
-    
+    <BrowserRouter>
       <Navibar />
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+      
+      </BrowserRouter>  
     </>
   );
 }
