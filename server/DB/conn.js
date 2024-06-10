9e287f6d57bfa02d3d4f2ceb43cc5fc5cc29d45e
Amansinghal904 <amansinghal904@gmail.com>
2024-06-10 20:26:12 +0530

    connected database to node.js and installed nodemon

---
+++ server/DB/conn.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const DB = "mongodb+srv://amansinghal904:aman2002@cluster0.3c7mdnm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+mongoose.connect(DB, {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("connection started")).catch((error) => console.log(error.message));
--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("./DB/conn");
+
 
 const port = 8003;
 
