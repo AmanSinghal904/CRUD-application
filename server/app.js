f8af3e97ec6012db589a0b1ac06461c5dfbdf1d0
Amansinghal904 <amansinghal904@gmail.com>
2024-06-10 20:44:40 +0530

    created user schema

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("./DB/conn");
+const users = require("./models/userSchema");
 
 
 const port = 8003;
