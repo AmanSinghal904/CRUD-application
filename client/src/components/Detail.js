6ed5bd37d2044962ce9878b430f0bf39c97aca96
Amansinghal904 <amansinghal904@gmail.com>
2024-06-11 16:39:18 +0530

    added delete and edit functionality in detail page

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -8,9 +8,9 @@ import CardContent from '@mui/material/CardContent';
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 
 import LocationOn from '@mui/icons-material/LocationOn';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams,useNavigate } from 'react-router-dom';
 const Detail = () => {
-
+    const navigate = useNavigate("");
     const { id } = useParams("");
     console.log(id)
 
@@ -45,14 +45,34 @@ const Detail = () => {
        getdata(); 
     },[])
 
+
+    const deleteuser = async(id)=>{
+        const res2 = await fetch(`/deleteuser/${id}`,{
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+    
+        const deletedata = await res2.json();
+        console.log(deletedata)
+        if(res2.status === 422 || !deletedata){
+          console.log("error");
+        }else{
+            console.log("data deleted");
+            
+            navigate("/")
+        }
+      }
+
     return (
         <div className='container mt-3'>
             <h1> welcome </h1>
             <Card sx={{ maxWidth: 600 }}>
                 <CardContent>
                     <div className='add_btn'>
-                        <button className='btn btn-primary mx-2'><CreateIcon /></button>
-                        <button className='btn btn-danger mx-2'><DeleteIcon /></button>
+                        <NavLink to={`/edit/${getuserdata._id}`}><button className='btn btn-primary mx-2'><CreateIcon /></button></NavLink>
+                        <button onClick = {()=>deleteuser(getuserdata._id)} className='btn btn-danger mx-2'><DeleteIcon /></button>
                     </div>
                     <div className="row">
                         <div className="leftview col-lg-6 col-md-6 col-12" >
