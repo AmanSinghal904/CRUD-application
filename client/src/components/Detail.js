20e8052c33165041500034b89fd6d6363f7ee67f
Amansinghal904 <amansinghal904@gmail.com>
2024-06-11 15:24:53 +0530

    integrated individual detail API with frontend

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CreateIcon from '@mui/icons-material/Create';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Card from '@mui/material/Card';
@@ -8,7 +8,43 @@ import CardContent from '@mui/material/CardContent';
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 
 import LocationOn from '@mui/icons-material/LocationOn';
+import { useParams } from 'react-router-dom';
 const Detail = () => {
+
+    const { id } = useParams("");
+    console.log(id)
+
+    const [getuserdata, setUserdata] = useState([]);
+    console.log(getuserdata)
+
+    const getdata = async () => {
+
+
+        const res = await fetch(`/getuser/${id}`, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+
+
+        });
+
+        const data = await res.json();
+        
+
+        if (res.status === 422 || !data) {
+            console.log("error");
+        } else {
+            setUserdata(data)
+            console.log("get data");
+        }
+
+    }
+    useEffect(()=>{
+       getdata(); 
+    },[])
+
     return (
         <div className='container mt-3'>
             <h1> welcome </h1>
@@ -20,17 +56,17 @@ const Detail = () => {
                     </div>
                     <div className="row">
                         <div className="leftview col-lg-6 col-md-6 col-12" >
-                            <h3 className='mt-3'> Name: <span>aman singhal</span></h3>
-                            <h3 className='mt-3'> Age: <span>21</span></h3>
+                            <h3 className='mt-3'> Name: <span>{getuserdata.name}</span></h3>
+                            <h3 className='mt-3'> Age: <span>{getuserdata.age}</span></h3>
 
-                            <p className='mt-3'><EmailIcon /> Email: <span> amansinghal904@gmail.com</span> </p>
-                            <p className='mt-3'> <WorkIcon /> Occupation: <span> Dev</span> </p>
+                            <p className='mt-3'><EmailIcon /> Email: <span> {getuserdata.email}</span> </p>
+                            <p className='mt-3'> <WorkIcon /> Occupation: <span> {getuserdata.work}</span> </p>
                         </div>
                         <div className="rightview col-lg-6 col-md-6 col-12" >
 
-                            <p className='mt-4'><PhoneAndroidIcon /> <span>7073578125</span></p>
-                            <p className='mt-4'><LocationOn /> <span>jaipur</span></p>
-                            <p className='mt-4'>Description: <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis aperiam, quos nulla voluptatibus earum facilis sunt consectetur eius hic ut.</span></p>
+                            <p className='mt-4'><PhoneAndroidIcon /> <span>{getuserdata.mobile}</span></p>
+                            <p className='mt-4'><LocationOn /> <span>{getuserdata.address}</span></p>
+                            <p className='mt-4'>Description: <span>{getuserdata.desc}</span></p>
                         </div>
                     </div>
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,6 +3,7 @@ import Table from 'react-bootstrap/Table';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
 import CreateIcon from '@mui/icons-material/Create';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
 
@@ -25,7 +26,7 @@ const Home = () => {
     const data = await res.json();
     console.log(data)
 
-    if (res.status === 404 || !data) {
+    if (res.status === 422 || !data) {
       console.log("error");
     } else {
       setUserdata(data)
@@ -68,7 +69,7 @@ const Home = () => {
                     <td>{element.work}</td>
                     <td>{element.mobile}</td>
                     <td className='d-flex justify-content-between'>
-                      <button className='btn btn-success'> <RemoveRedEyeIcon /></button>
+                      <NavLink to={`view/${element._id}`}><button className='btn btn-success'> <RemoveRedEyeIcon /></button></NavLink>
                       <button className='btn btn-primary'><CreateIcon /></button>
                       <button className='btn btn-danger'><DeleteIcon /></button>
                     </td>
