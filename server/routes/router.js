1eaaf3cfeeb8d4e9fb39ce57a64ec796782da665
Amansinghal904 <amansinghal904@gmail.com>
2024-06-11 16:13:44 +0530

    created deleteuser API

--- server/routes/router.js
+++ server/routes/router.js
@@ -8,24 +8,24 @@ const users = require("../models/userSchema")
 // });
 
 //register user
-router.post("/register",async(req,res)=>{
+router.post("/register", async (req, res) => {
     // console.log(req.body);
-    const {name,email,age,mobile,work,address,desc}= req.body;
+    const { name, email, age, mobile, work, address, desc } = req.body;
 
-    if(!name||!email|| !age||!mobile||!work||!address||!desc){
+    if (!name || !email || !age || !mobile || !work || !address || !desc) {
         res.status(422).json("please fill the data");
     }
     try {
-        
-        const preuser = await users.findOne({email:email});
+
+        const preuser = await users.findOne({ email: email });
         console.log(preuser);
 
-        if(preuser){
+        if (preuser) {
             res.status(422).json("this user is already present");
-            
-        }else{
+
+        } else {
             const adduser = new users({
-                name,email,age,mobile,work,address,desc
+                name, email, age, mobile, work, address, desc
             });
             await adduser.save();
             res.status(201).json(adduser)
@@ -38,7 +38,7 @@ router.post("/register",async(req,res)=>{
 })
 //get userdata
 
-router.get("/getdata",async(req,res)=>{
+router.get("/getdata", async (req, res) => {
     try {
         const userdata = await users.find();
         res.status(201).json(userdata)
@@ -48,12 +48,12 @@ router.get("/getdata",async(req,res)=>{
     }
 })
 // get individual user detail
-router.get("/getuser/:id",async(req,res)=>{
+router.get("/getuser/:id", async (req, res) => {
     try {
         console.log(req.params);
-        const {id} = req.params;
+        const { id } = req.params;
 
-        const userindividual = await users.findById({_id:id});
+        const userindividual = await users.findById({ _id: id });
         console.log(userindividual);
         res.status(201).json(userindividual)
     } catch (error) {
@@ -61,12 +61,12 @@ router.get("/getuser/:id",async(req,res)=>{
     }
 })
 //update user data
-router.patch("/updateuser/:id",async(req,res)=>{
+router.patch("/updateuser/:id", async (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
 
-        const updateduser = await users.findByIdAndUpdate(id,req.body,{
-            new:true
+        const updateduser = await users.findByIdAndUpdate(id, req.body, {
+            new: true
         });
         console.log(updateduser)
         res.status(201).json(updateduser);
@@ -75,4 +75,16 @@ router.patch("/updateuser/:id",async(req,res)=>{
     }
 })
 
-module.exports=router;
+//delete user
+router.delete("/deleteuser/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deleteuser = await users.findByIdAndDelete({_id:id});
+        console.log(deleteuser)
+        res.status(201).json(deleteuser);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+module.exports = router;
